f825c3683d5a03dc4709b54c95ddd84b2636ca00
balde <balde@balde>
2017-05-17 22:12:36 +0200

    Ajout des attributs dans l'entite user

--- FoodFast/src/App/PlatformBundle/Entity/user.php
+++ FoodFast/src/App/PlatformBundle/Entity/user.php
@@ -27,6 +27,55 @@ class user
      * @ORM\Column(name="nom", type="string", length=1000)
      */
     private $nom;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=1000)
+     */
+    private $prenom;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=1000)
+     */
+    private $email;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="passwrd", type="text")
+     */
+    private $passwrd;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adresse", type="text")
+     */
+    private $adresse;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code_postal", type="integer")
+     */
+    private $codePostal;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="telephone", type="integer")
+     */
+    private $telephone;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type_user", type="string", length=8)
+     */
+    private $typeUser;
 
 
     /**
@@ -61,5 +110,173 @@ class user
     public function getNom()
     {
         return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return user
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return user
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set passwrd
+     *
+     * @param string $passwrd
+     *
+     * @return user
+     */
+    public function setPasswrd($passwrd)
+    {
+        $this->passwrd = $passwrd;
+
+        return $this;
+    }
+
+    /**
+     * Get passwrd
+     *
+     * @return string
+     */
+    public function getPasswrd()
+    {
+        return $this->passwrd;
+    }
+
+    /**
+     * Set adresse
+     *
+     * @param string $adresse
+     *
+     * @return user
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    /**
+     * Get adresse
+     *
+     * @return string
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * Set codePostal
+     *
+     * @param integer $codePostal
+     *
+     * @return user
+     */
+    public function setCodePostal($codePostal)
+    {
+        $this->codePostal = $codePostal;
+
+        return $this;
+    }
+
+    /**
+     * Get codePostal
+     *
+     * @return integer
+     */
+    public function getCodePostal()
+    {
+        return $this->codePostal;
+    }
+
+    /**
+     * Set telephone
+     *
+     * @param integer $telephone
+     *
+     * @return user
+     */
+    public function setTelephone($telephone)
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    /**
+     * Get telephone
+     *
+     * @return integer
+     */
+    public function getTelephone()
+    {
+        return $this->telephone;
+    }
+
+    /**
+     * Set typeUser
+     *
+     * @param string $typeUser
+     *
+     * @return user
+     */
+    public function setTypeUser($typeUser)
+    {
+        $this->typeUser = $typeUser;
+
+        return $this;
+    }
+
+    /**
+     * Get typeUser
+     *
+     * @return string
+     */
+    public function getTypeUser()
+    {
+        return $this->typeUser;
     }
 }
