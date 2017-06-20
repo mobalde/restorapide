28fecf8f9a06d9bec729f6b68779b3938cdc7d30
mobalde <mamoudous2005@yahoo.fr>
2017-06-20 17:32:52 +0200

    creation d'ajout de nouvelle taille et prix d'un produit

--- FoodFast/src/App/AdminBundle/Resources/public/js/gestion-admin.js
+++ FoodFast/src/App/AdminBundle/Resources/public/js/gestion-admin.js
@@ -3,11 +3,18 @@
  * Affiche le bon formulaire par rapport à l'item selectionné dans le menu
  */
 
-document.getElementById('new-bl-heightPrice').addEventListener('click', function( event ) {
+document.getElementById('add-price-witdth').addEventListener('click', function( event ) {
     // Affiche le compte courant de clics à l'intérieur de la div cliquée
-    var nbelement = document.querySelectorAll('new-bl-heightPrice div'); // le nombre de bloc
+    var nbelement = document.querySelectorAll('#new-bl-heightPrice div'); // le nombre de bloc
+    console.log('-------- nbelement: '+nbelement.length);
     if(nbelement.length<=2){
-        document.getElementById('new-bl-heightPrice').innerHTML += '<input type="text" >';
+        // creation du bloc
+        var newbloc = document.createElement('div');
+        newbloc.className = "form-group";
+        newbloc.innerHTML = '<label for="app_platformbundle_produit_taille'+(nbelement.length+1)+'" class="required">Taille *</label>'+
+           '<select id="app_platformbundle_produit_taille'+(nbelement.length+1)+'" name="app_platformbundle_produit_taille'+(nbelement.length+1)+'" required="required" class="form-control taille"><option value="" selected="selected">Selectionner la taille</option><option value="mega">mega</option><option value="senior">senior</option><option value="junior">junior</option></select>';
+        
+        document.getElementById('new-bl-heightPrice').insertBefore(newbloc, document.getElementById("bloc-btn-new-t-p")); // Ajout du nouveau bloc dans le bloc principal
     }
     else{
         // On desactive le bouton 
